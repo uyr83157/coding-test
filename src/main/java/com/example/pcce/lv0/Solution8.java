34d82192b5ae952b98a29f30394601e21b8d70fa
uyr83157 <uyr83157@gmail.com>
2025-05-16 21:23:06 +0900

    coding test

---
+++ src/main/java/com/example/pcce/lv0/Solution8.java
@@ -0,0 +1,33 @@
+package com.example.pcce.lv0;
+
+public class Solution8 {
+	// [PCCE 기출문제] 8번 / 닉네임 규칙
+
+	public String solution(String nickname) {
+		String answer = "";
+		for(int i=0; i<nickname.length(); i++){
+			if(nickname.charAt(i) == 'l'){
+				answer += "I";
+			}
+			else if(nickname.charAt(i) == 'w'){
+				answer += "vv";
+			}
+			else if(nickname.charAt(i) == 'W'){
+				answer += "VV";
+			}
+			else if(nickname.charAt(i) == 'O'){
+				answer += "0";
+			}
+			else{
+				answer += nickname.charAt(i);
+			}
+		}
+		while(answer.length() <= 3){
+			answer += "o";
+		}
+		if(answer.length() > 8){
+			answer = answer.substring(0, 8);
+		}
+		return answer;
+	}
+}
